8bb2208a500774cf15b373c3aecff07664aa677b
fernando@abcdataz.com <fernando@abcdataz.com>
2024-09-19 08:40:44 +1200

    fixed item

--- transformation/models/normalized/xero_items.sql
+++ transformation/models/normalized/xero_items.sql
@@ -12,12 +12,6 @@ WITH items_raw AS (
         SAFE_CAST(JSON_VALUE(data, '$.IsPurchased') AS BOOL) AS is_purchased,
         JSON_VALUE(data, '$.Description') AS description,
         JSON_VALUE(data, '$.PurchaseDescription') AS purchase_description,
-        JSON_VALUE(sales_detail, '$.AccountCode') AS sales_detail_account_code,
-        JSON_VALUE(sales_detail, '$.TaxType') AS sales_detail_tax_type,
-        CAST(JSON_VALUE(sales_detail, '$.UnitPrice') AS NUMERIC) AS sales_detail_unit_price,
-        JSON_VALUE(purchase_detail, '$.AccountCode') AS purchase_detail_account_code,
-        JSON_VALUE(purchase_detail, '$.TaxType') AS purchase_detail_tax_type,
-        CAST(JSON_VALUE(purchase_detail, '$.UnitPrice') AS NUMERIC) AS purchase_detail_unit_price,
         SAFE_CAST(JSON_VALUE(data, '$.IsTrackedAsInventory') AS BOOL) AS is_tracked_as_inventory,
         JSON_VALUE(data, '$.InventoryAssetAccountCode') AS inventory_asset_account_code,
         CAST(JSON_VALUE(data, '$.TotalCostPool') AS NUMERIC) AS total_cost_pool,
@@ -26,14 +20,15 @@ WITH items_raw AS (
             CAST(
                 REGEXP_EXTRACT(JSON_VALUE(data, '$.UpdatedDateUTC'), r'/Date\((\d+)\+\d+\)/') AS INT64
             )
-        ) AS updated_date_utc
-        
+        ) AS updated_date_utc,
+        JSON_VALUE(data, '$.SalesDetails.AccountCode') AS sales_detail_account_code,
+        JSON_VALUE(data, '$.SalesDetails.TaxType') AS sales_detail_tax_type,
+        CAST(JSON_VALUE(data, '$.SalesDetails.UnitPrice') AS NUMERIC) AS sales_detail_unit_price,
+        JSON_VALUE(data, '$.PurchaseDetails.AccountCode') AS purchase_detail_account_code,
+        JSON_VALUE(data, '$.PurchaseDetails.TaxType') AS purchase_detail_tax_type,
+        CAST(JSON_VALUE(data, '$.PurchaseDetails.UnitPrice') AS NUMERIC) AS purchase_detail_unit_price
     FROM 
-        {{ source('raw', 'xero_items') }},
-        UNNEST(JSON_EXTRACT_ARRAY(data, '$.SalesDetails')) AS sales_detail
-    LEFT JOIN
-        UNNEST(JSON_EXTRACT_ARRAY(data, '$.PurchaseDetails')) AS purchase_detail
-        ON TRUE
+        {{ source('raw', 'xero_items') }}
 )
 
 SELECT
@@ -45,16 +40,16 @@ SELECT
     is_purchased,
     description,
     purchase_description,
+    is_tracked_as_inventory,
+    inventory_asset_account_code,
+    total_cost_pool,
+    quantity_on_hand,
+    updated_date_utc,
     sales_detail_account_code,
     sales_detail_tax_type,
     sales_detail_unit_price,
     purchase_detail_account_code,
     purchase_detail_tax_type,
-    purchase_detail_unit_price,
-    is_tracked_as_inventory,
-    inventory_asset_account_code,
-    total_cost_pool,
-    quantity_on_hand,
-    updated_date_utc
+    purchase_detail_unit_price
 FROM 
     items_raw
