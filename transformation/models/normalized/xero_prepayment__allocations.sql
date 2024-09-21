96bec62c198973e6871badd754c67ef674b03463
fernando@abcdataz.com <fernando@abcdataz.com>
2024-09-21 23:25:18 +1200

    remodeled prepayment

--- transformation/models/normalized/xero_prepayment__allocations.sql
+++ transformation/models/normalized/xero_prepayment__allocations.sql
@@ -1,25 +1,29 @@
 {{ config(
-    tags=['normalized', 'xero', 'prepayment_allocations']
+    tags=['normalized', 'xero', 'prepayments', 'prepayment__allocations']
 ) }}
 
 WITH allocations_raw AS (
-    SELECT
+    SELECT DISTINCT
+        ingestion_time,
         JSON_VALUE(data, '$.PrepaymentID') AS prepayment_id,
-        SAFE_CAST(NULLIF(JSON_VALUE(allocation.value, '$.Amount'), '') AS NUMERIC) AS allocation_amount,
+        JSON_VALUE(allocation, '$.AllocationID') AS allocation_id,
+        JSON_VALUE(allocation, '$.Amount') AS allocation_amount,
         TIMESTAMP_MILLIS(
             CAST(
-                REGEXP_EXTRACT(JSON_VALUE(allocation.value, '$.Date'), r'/Date\((\d+)\+\d+\)/') AS INT64
+                REGEXP_EXTRACT(JSON_VALUE(allocation, '$.Date'), r'/Date\((\d+)\+\d+\)/') AS INT64
             )
         ) AS allocation_date,
-        JSON_VALUE(allocation.value, '$.Invoice.InvoiceID') AS invoice_id,
-        JSON_VALUE(allocation.value, '$.Invoice.InvoiceNumber') AS invoice_number
+        JSON_VALUE(allocation, '$.Invoice.InvoiceID') AS invoice_id,
+        JSON_VALUE(allocation, '$.Invoice.InvoiceNumber') AS invoice_number
     FROM 
         {{ source('raw', 'xero_prepayments') }},
         UNNEST(JSON_EXTRACT_ARRAY(data, '$.Allocations')) AS allocation
 )
 
 SELECT
+    ingestion_time,
     prepayment_id,
+    allocation_id,
     allocation_amount,
     allocation_date,
     invoice_id,
--- transformation/models/normalized/xero_prepayments.sql
+++ transformation/models/normalized/xero_prepayments.sql
@@ -3,55 +3,59 @@
 ) }}
 
 WITH prepayments_raw AS (
-    SELECT
+    SELECT DISTINCT
         ingestion_time,
         JSON_VALUE(data, '$.PrepaymentID') AS prepayment_id,
-        JSON_VALUE(data, '$.Type') AS type,
-        JSON_VALUE(data, '$.Date') AS prepayment_date,
+        JSON_VALUE(data, '$.Contact.ContactID') AS contact_id,
+        JSON_VALUE(data, '$.Contact.Name') AS contact_name,
+        TIMESTAMP_MILLIS(
+            CAST(
+                REGEXP_EXTRACT(JSON_VALUE(data, '$.Date'), r'/Date\((\d+)\+\d+\)/') AS INT64
+            )
+        ) AS date,
         JSON_VALUE(data, '$.Status') AS status,
         JSON_VALUE(data, '$.LineAmountTypes') AS line_amount_types,
         SAFE_CAST(JSON_VALUE(data, '$.SubTotal') AS NUMERIC) AS subtotal,
         SAFE_CAST(JSON_VALUE(data, '$.TotalTax') AS NUMERIC) AS total_tax,
         SAFE_CAST(JSON_VALUE(data, '$.Total') AS NUMERIC) AS total,
+        TIMESTAMP_MILLIS(
+            CAST(
+                REGEXP_EXTRACT(JSON_VALUE(data, '$.UpdatedDateUTC'), r'/Date\((\d+)\+\d+\)/') AS INT64
+            )
+        ) AS updated_date_utc,
         JSON_VALUE(data, '$.CurrencyCode') AS currency_code,
         SAFE_CAST(JSON_VALUE(data, '$.CurrencyRate') AS NUMERIC) AS currency_rate,
         JSON_VALUE(data, '$.Reference') AS reference,
         SAFE_CAST(JSON_VALUE(data, '$.RemainingCredit') AS NUMERIC) AS remaining_credit,
         TIMESTAMP_MILLIS(
             CAST(
-                REGEXP_EXTRACT(JSON_VALUE(data, '$.UpdatedDateUTC'), r'/Date\((\d+)\+\d+\)/') AS INT64
-            )
-        ) AS updated_date_utc,
-        TIMESTAMP_MILLIS(
-            CAST(
                 REGEXP_EXTRACT(JSON_VALUE(data, '$.FullyPaidOnDate'), r'/Date\((\d+)\+\d+\)/') AS INT64
             )
         ) AS fully_paid_on_date,
-        SAFE_CAST(JSON_VALUE(data, '$.HasAttachments') AS BOOL) AS has_attachments,
-        JSON_VALUE(data, '$.Contact.ContactID') AS contact_id,
-        JSON_VALUE(data, '$.Contact.Name') AS contact_name
+        JSON_VALUE(data, '$.Type') AS type,
+        SAFE_CAST(JSON_VALUE(data, '$.HasAttachments') AS BOOL) AS has_attachments
     FROM 
         {{ source('raw', 'xero_prepayments') }}
 )
 
 SELECT
+    ingestion_time,
     prepayment_id,
-    type,
-    prepayment_date,
+    contact_id,
+    contact_name,
+    date,
     status,
     line_amount_types,
     subtotal,
     total_tax,
     total,
+    updated_date_utc,
     currency_code,
     currency_rate,
     reference,
     remaining_credit,
-    updated_date_utc,
     fully_paid_on_date,
-    has_attachments,
-    contact_id,
-    contact_name,
-    ingestion_time
+    type,
+    has_attachments
 FROM 
   prepayments_raw
