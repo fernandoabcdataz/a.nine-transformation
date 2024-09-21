da0f19c64bf225506c48fa1757f2a307780942eb
fernando@abcdataz.com <fernando@abcdataz.com>
2024-09-21 13:30:24 +1200

    remodeled xero_batch_payments

--- transformation/models/normalized/xero_batch_payment__payments.sql
+++ transformation/models/normalized/xero_batch_payment__payments.sql
@@ -1,5 +1,5 @@
 {{ config(
-    tags=['normalized', 'xero', 'batch_payment__payments']
+    tags=['normalized', 'xero', 'batch_payments', 'batch_payment__payments']
 ) }}
 
 WITH batch_payment_payments_raw AS (
@@ -12,7 +12,8 @@ WITH batch_payment_payments_raw AS (
         JSON_VALUE(payment, '$.Particulars') AS payment_particulars,
         JSON_VALUE(payment, '$.Code') AS payment_code,
         JSON_VALUE(payment, '$.Reference') AS payment_reference,
-        CAST(JSON_VALUE(payment, '$.Amount') AS NUMERIC) AS amount
+        JSON_VALUE(payment, '$.Details') AS payment_details,
+        SAFE_CAST(JSON_VALUE(payment, '$.Amount') AS NUMERIC) AS amount
     FROM 
         {{ source('raw', 'xero_batch_payments') }},
         UNNEST(JSON_EXTRACT_ARRAY(data, '$.Payments')) AS payment
@@ -20,13 +21,14 @@ WITH batch_payment_payments_raw AS (
 
 SELECT
     ingestion_time,
-    batch_payment_id,
+    JSON_VALUE(data, '$.BatchPaymentID') AS batch_payment_id,
     invoice_id,
     payment_id,
     bank_account_number,
     payment_particulars,
     payment_code,
     payment_reference,
+    payment_details,
     amount
 FROM 
     batch_payment_payments_raw
--- transformation/models/normalized/xero_batch_payments.sql
+++ transformation/models/normalized/xero_batch_payments.sql
@@ -6,26 +6,26 @@ WITH batch_payments_raw AS (
     SELECT
         ingestion_time,
         JSON_VALUE(data, '$.BatchPaymentID') AS batch_payment_id,
-        JSON_VALUE(data, '$.Account.AccountID') AS account_id,        
+        JSON_VALUE(data, '$.Account.AccountID') AS account_id,
         JSON_VALUE(data, '$.Particulars') AS particulars, -- NZ only fields
         JSON_VALUE(data, '$.Code') AS code, -- NZ only fields
         JSON_VALUE(data, '$.Reference') AS reference, -- NZ only fields
         -- JSON_VALUE(data, '$.Details') AS details, -- Non-NZ only fields
-        JSON_VALUE(data, '$.Narrative') AS narrative, -- UK only fields
+        -- JSON_VALUE(data, '$.Narrative') AS narrative, -- UK only fields
         TIMESTAMP_MILLIS(
             CAST(
                 SAFE.REGEXP_EXTRACT(JSON_VALUE(data, '$.Date'), r'/Date\((\d+)\+\d+\)/') AS INT64
             )
-        ) AS date, -- parsing Date
+        ) AS date,
         JSON_VALUE(data, '$.Type') AS type,
         JSON_VALUE(data, '$.Status') AS status,
-        CAST(JSON_VALUE(data, '$.TotalAmount') AS NUMERIC) AS total_amount,
-        CAST(JSON_VALUE(data, '$.IsReconciled') AS BOOL) AS is_reconciled,
+        SAFE_CAST(JSON_VALUE(data, '$.TotalAmount') AS NUMERIC) AS total_amount,
+        SAFE_CAST(JSON_VALUE(data, '$.IsReconciled') AS BOOL) AS is_reconciled,
         TIMESTAMP_MILLIS(
             CAST(
                 SAFE.REGEXP_EXTRACT(JSON_VALUE(data, '$.UpdatedDateUTC'), r'/Date\((\d+)\+\d+\)/') AS INT64
             )
-        ) AS updated_date_utc -- parsing UpdatedDateUTC
+        ) AS updated_date_utc
     FROM 
         {{ source('raw', 'xero_batch_payments') }}
 )
@@ -37,8 +37,6 @@ SELECT
     particulars,
     code,
     reference,
-    -- details,
-    narrative,
     date,
     type,
     status,
