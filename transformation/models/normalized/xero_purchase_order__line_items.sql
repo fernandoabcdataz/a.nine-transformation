96e6c240f1a073e5a2ced4191dc3bda20930248a
fernando@abcdataz.com <fernando@abcdataz.com>
2024-09-22 13:49:45 +1200

    updated purchase_orders

--- transformation/models/normalized/xero_purchase_order__line_items.sql
+++ transformation/models/normalized/xero_purchase_order__line_items.sql
@@ -1,9 +1,10 @@
 {{ config(
-    tags=['normalized', 'xero', 'purchase_order_line_items']
+    tags=['normalized', 'xero', 'purchase_orders', 'purchase_order__line_items']
 ) }}
 
 WITH line_items_raw AS (
-    SELECT
+    SELECT DISTINCT
+        ingestion_time,
         JSON_VALUE(data, '$.PurchaseOrderID') AS purchase_order_id,
         JSON_VALUE(line_item.value, '$.LineItemID') AS line_item_id,
         JSON_VALUE(line_item.value, '$.Description') AS description,
@@ -12,16 +13,17 @@ WITH line_items_raw AS (
         JSON_VALUE(line_item.value, '$.ItemCode') AS item_code,
         JSON_VALUE(line_item.value, '$.AccountCode') AS account_code,
         JSON_VALUE(line_item.value, '$.TaxType') AS tax_type,
+        SAFE_CAST(JSON_VALUE(line_item.value, '$.DiscountRate') AS NUMERIC) AS discount_rate,
         SAFE_CAST(JSON_VALUE(line_item.value, '$.TaxAmount') AS NUMERIC) AS tax_amount,
         SAFE_CAST(JSON_VALUE(line_item.value, '$.LineAmount') AS NUMERIC) AS line_amount,
-        SAFE_CAST(JSON_VALUE(line_item.value, '$.DiscountRate') AS NUMERIC) AS discount_rate,
-        JSON_VALUE(line_item.value, '$.TrackingCategory') AS tracking_category
+        JSON_VALUE(line_item.value, '$.Tracking') AS tracking -- temporary
     FROM 
         {{ source('raw', 'xero_purchase_orders') }},
         UNNEST(JSON_EXTRACT_ARRAY(data, '$.LineItems')) AS line_item
 )
 
 SELECT
+    ingestion_time,
     purchase_order_id,
     line_item_id,
     description,
@@ -30,9 +32,9 @@ SELECT
     item_code,
     account_code,
     tax_type,
+    discount_rate,
     tax_amount,
     line_amount,
-    discount_rate,
-    tracking_category
+    tracking -- temporary
 FROM 
     line_items_raw
--- transformation/models/normalized/xero_purchase_orders.sql
+++ transformation/models/normalized/xero_purchase_orders.sql
@@ -3,78 +3,77 @@
 ) }}
 
 WITH purchase_orders_raw AS (
-    SELECT
+    SELECT DISTINCT
         ingestion_time,
         JSON_VALUE(data, '$.PurchaseOrderID') AS purchase_order_id,
+        JSON_VALUE(data, '$.PurchaseOrderNumber') AS purchase_order_number,
         JSON_VALUE(data, '$.Contact.ContactID') AS contact_id,
         JSON_VALUE(data, '$.Contact.Name') AS contact_name,
-        JSON_VALUE(data, '$.Date') AS purchase_order_date,
-        JSON_VALUE(data, '$.DeliveryDate') AS delivery_date,
+        TIMESTAMP_MILLIS(
+            CAST(
+                REGEXP_EXTRACT(JSON_VALUE(data, '$.Date'), r'/Date\((\d+)\+\d+\)/') AS INT64
+            )
+        ) AS date,
+        TIMESTAMP_MILLIS(
+            CAST(
+                REGEXP_EXTRACT(JSON_VALUE(data, '$.DeliveryDate'), r'/Date\((\d+)\+\d+\)/') AS INT64
+            )
+        ) AS delivery_date,
         JSON_VALUE(data, '$.LineAmountTypes') AS line_amount_types,
-        JSON_VALUE(data, '$.PurchaseOrderNumber') AS purchase_order_number,
         JSON_VALUE(data, '$.Reference') AS reference,
         JSON_VALUE(data, '$.BrandingThemeID') AS branding_theme_id,
         JSON_VALUE(data, '$.CurrencyCode') AS currency_code,
         JSON_VALUE(data, '$.Status') AS status,
-        JSON_VALUE(data, '$.ExpectedArrivalDate') AS expected_arrival_date,
+        JSON_VALUE(data, '$.SentToContact') AS sent_to_contact,
         JSON_VALUE(data, '$.DeliveryAddress') AS delivery_address,
+        JSON_VALUE(data, '$.AttentionTo') AS attention_to,
+        JSON_VALUE(data, '$.Telephone') AS telephone,
+        JSON_VALUE(data, '$.DeliveryInstructions') AS delivery_instructions,
+        TIMESTAMP_MILLIS(
+            CAST(
+                REGEXP_EXTRACT(JSON_VALUE(data, '$.ExpectedArrivalDate'), r'/Date\((\d+)\+\d+\)/') AS INT64
+            )
+        ) AS expected_arrival_date,
+        SAFE_CAST(JSON_VALUE(data, '$.CurrencyRate') AS NUMERIC) AS currency_rate,
         SAFE_CAST(JSON_VALUE(data, '$.SubTotal') AS NUMERIC) AS subtotal,
         SAFE_CAST(JSON_VALUE(data, '$.TotalTax') AS NUMERIC) AS total_tax,
         SAFE_CAST(JSON_VALUE(data, '$.Total') AS NUMERIC) AS total,
         SAFE_CAST(JSON_VALUE(data, '$.TotalDiscount') AS NUMERIC) AS total_discount,
-        SAFE_CAST(JSON_VALUE(data, '$.CurrencyRate') AS NUMERIC) AS currency_rate,
-        JSON_VALUE(data, '$.AttentionTo') AS attention_to,
-        JSON_VALUE(data, '$.Telephone') AS telephone,
-        JSON_VALUE(data, '$.DeliveryInstructions') AS delivery_instructions,
         SAFE_CAST(JSON_VALUE(data, '$.HasAttachments') AS BOOL) AS has_attachments,
         TIMESTAMP_MILLIS(
             CAST(
                 REGEXP_EXTRACT(JSON_VALUE(data, '$.UpdatedDateUTC'), r'/Date\((\d+)\+\d+\)/') AS INT64
             )
-        ) AS updated_date_utc,
-        TIMESTAMP_MILLIS(
-            CAST(
-                REGEXP_EXTRACT(JSON_VALUE(data, '$.Date'), r'/Date\((\d+)\+\d+\)/') AS INT64
-            )
-        ) AS parsed_purchase_order_date,
-        TIMESTAMP_MILLIS(
-            CAST(
-                REGEXP_EXTRACT(JSON_VALUE(data, '$.DeliveryDate'), r'/Date\((\d+)\+\d+\)/') AS INT64
-            )
-        ) AS parsed_delivery_date,
-        TIMESTAMP_MILLIS(
-            CAST(
-                REGEXP_EXTRACT(JSON_VALUE(data, '$.ExpectedArrivalDate'), r'/Date\((\d+)\+\d+\)/') AS INT64
-            )
-        ) AS parsed_expected_arrival_date
+        ) AS updated_date_utc
     FROM 
         {{ source('raw', 'xero_purchase_orders') }}
 )
 
 SELECT
+ingestion_time,
     purchase_order_id,
     purchase_order_number,
-    parsed_purchase_order_date AS purchase_order_date,
-    parsed_delivery_date AS delivery_date,
-    parsed_expected_arrival_date AS expected_arrival_date,
-    status,
+    contact_id,
+    contact_name,
+    date,
+    delivery_date,
     line_amount_types,
-    subtotal,
-    total_tax,
-    total,
-    total_discount,
     reference,
+    branding_theme_id,
     currency_code,
-    currency_rate,
+    status,
+    sent_to_contact,
     delivery_address,
     attention_to,
     telephone,
     delivery_instructions,
-    branding_theme_id,
+    expected_arrival_date,
+    currency_rate,
+    subtotal,
+    total_tax,
+    total,
+    total_discount,
     has_attachments,
-    contact_id,
-    contact_name,
-    updated_date_utc,
-    ingestion_time
+    updated_date_utc
 FROM 
     purchase_orders_raw
